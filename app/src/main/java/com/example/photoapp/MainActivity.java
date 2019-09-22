ec15a697a58440f8fe1ab2085294a73e0b34c993
chaoswlz <wulize1994@gmail.com>
2019-09-22 14:40:12 -0700

    make camera work

--- app/src/main/java/com/example/photoapp/MainActivity.java
+++ app/src/main/java/com/example/photoapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.photoapp;
 
+
 import android.os.Bundle;
+
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -9,6 +13,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
     }
 
 }
--- app/src/main/java/com/example/photoapp/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/photoapp/ui/dashboard/DashboardFragment.java
@@ -1,33 +1,41 @@
 package com.example.photoapp.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import androidx.annotation.Nullable;
+import android.widget.Button;
+
+
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
+
 import androidx.lifecycle.ViewModelProviders;
+
+import com.example.photoapp.PhotoActivity;
 import com.example.photoapp.R;
 
 public class DashboardFragment extends Fragment {
-
+    private Button photoButton;
     private DashboardViewModel dashboardViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
+        photoButton = root.findViewById(R.id.photoButton);
+        photoButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
             }
         });
+
         return root;
     }
 }
