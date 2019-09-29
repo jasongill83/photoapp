7ea007ec6137c7f8d998216b40610cd3f86cd35d
chaoswlz <wulize1994@gmail.com>
2019-09-28 17:38:11 -0700

    fix bug that cause bitmap oom

--- app/src/main/java/com/example/photoapp/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/photoapp/ui/dashboard/DashboardFragment.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -30,28 +31,72 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import static android.app.Activity.RESULT_OK;
 
 public class DashboardFragment extends Fragment {
     private Button photoButton;
+    private Button rightButton;
+    private Button leftButton;
     private ImageView imageView;
     private DashboardViewModel dashboardViewModel;
     private File picture;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     String currentPhotoPath;
     static final int REQUEST_TAKE_PHOTO = 1;
+    int correntIndex = 0;
+    public ArrayList<String> pictureList = new ArrayList<String>();
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        getPictureList();
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
         photoButton = root.findViewById(R.id.photoButton);
+        rightButton = root.findViewById(R.id.rightButton);
+        leftButton = root.findViewById(R.id.leftButton);
+
         imageView = root.findViewById(R.id.imageView);
+
+        ViewTreeObserver vto = imageView.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                imageView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                if (pictureList.size() >0){
+                    setPic(pictureList.get(correntIndex));
+                }
+            }
+        });
+
+        rightButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (correntIndex+1>pictureList.size()){
+                    correntIndex = pictureList.size();
+                }
+                setPic(pictureList.get(correntIndex));
+            }
+        });
+
+        leftButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (correntIndex-1<0){
+                    correntIndex = 0;
+                }
+                setPic(pictureList.get(correntIndex));
+            }
+        });
+
         photoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,14 +122,31 @@ public class DashboardFragment extends Fragment {
             }
         });
 
+
+
         return root;
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            setPic();
+            setPic(currentPhotoPath);
         }
+    }
+
+    public void leftButtonAction(View v){
+        if (correntIndex-1<0){
+            correntIndex = 0;
+        }
+        setPic(pictureList.get(correntIndex));
+
+    }
+
+    public void rightButtonAction(View v){
+        if (correntIndex+1>pictureList.size()){
+            correntIndex = pictureList.size();
+        }
+        setPic(pictureList.get(correntIndex));
     }
 
     private File createImageFile() throws IOException {
@@ -111,7 +173,7 @@ public class DashboardFragment extends Fragment {
         getActivity().sendBroadcast(mediaScanIntent);
     }
 
-    private void setPic() {
+    private void setPic(String currentPhotoPath) {
         // Get the dimensions of the View
         int targetW = imageView.getWidth();
         int targetH = imageView.getHeight();
@@ -119,20 +181,45 @@ public class DashboardFragment extends Fragment {
         // Get the dimensions of the bitmap
         BitmapFactory.Options bmOptions = new BitmapFactory.Options();
         bmOptions.inJustDecodeBounds = true;
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
 
         int photoW = bmOptions.outWidth;
         int photoH = bmOptions.outHeight;
 
         // Determine how much to scale down the image
         int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        Log.println(Log.INFO,"info",scaleFactor+"");
 
         // Decode the image file into a Bitmap sized to fill the View
         bmOptions.inJustDecodeBounds = false;
         bmOptions.inSampleSize = scaleFactor;
         bmOptions.inPurgeable = true;
 
-        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
         imageView.setImageBitmap(bitmap);
+    }
+
+    public void getPictureList() {
+        ArrayList<String> list = new ArrayList<String>();
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File[] allfiles = storageDir.listFiles();
+        if (allfiles == null) {
+            return;
+        }
+        for(int k = 0; k < allfiles.length; k++) {
+            final File fi = allfiles[k];
+            if(fi.isFile()) {
+                int idx = fi.getPath().lastIndexOf(".");
+                if (idx <= 0) {
+                    continue;
+                }
+                String suffix = fi.getPath().substring(idx);
+                if (suffix.toLowerCase().equals(".jpg") || suffix.toLowerCase().equals(".jpeg") || suffix.toLowerCase().equals(".bmp") || suffix.toLowerCase().equals(".png") || suffix.toLowerCase().equals(".gif") ) {
+                    list.add(fi.getPath());
+                }
+            }
+        }
+        pictureList =  list;
     }
 
 }
